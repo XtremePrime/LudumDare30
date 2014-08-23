157706286705c6580e5c276320d4c79a8847369e
Aramande <aramande@hackular.com>
2014-08-23 09:21:04 +0200

    small update with most current cbp

--- LudumDare30/src/statemanager.h
+++ LudumDare30/src/statemanager.h
@@ -1,1 +1,11 @@
-statemanager.h
+#ifndef STATEMANAGER_H
+#define STATEMANAGER_H
+
+class StateManager {
+private:
+	std::vector<State> state_stack;
+public:
+	StateManager(/* args */) = default;
+};
+
+#endif // STATEMANAGER_H
