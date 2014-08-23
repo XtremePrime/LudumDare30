38be99c9201fd44ca90f725555e64a8b1d82e609
Aramande <aramande@hackular.com>
2014-08-23 09:28:32 +0200

    Fixed small issues with the statemanager

--- LudumDare30/src/statemanager.h
+++ LudumDare30/src/statemanager.h
@@ -3,9 +3,22 @@
 
 class StateManager {
 private:
-	std::vector<State> state_stack;
+	bool _running;
+	std::vector<State*> state_stack;
 public:
-	StateManager(/* args */) = default;
+	void init();
+	void cleanup();
+
+	void handle_events();
+	void update();
+	void draw();
+
+	void change_state(State* state);
+	void push_state(State* state);
+	void pop_state();
+
+	bool running() { return _running; }
+	void quit() { _running = false; }
 };
 
 #endif // STATEMANAGER_H
